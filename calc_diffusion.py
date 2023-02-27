8d3ddbc354f671457ecb25bb9a905396c83a4704
zwehrspan <zwehrspan@gmail.com>
2023-02-27 13:24:27 -0700

    Update calc_diffusion.py

--- calc_diffusion.py
+++ calc_diffusion.py
@@ -14,21 +14,21 @@ def calc_tau(val, mytime):
   return(out_tau)
 
 # Read in translational values
-iData = pd.read_csv("../DATA/translational_diff_coeff_molnum.txt", delim_whitespace=True, header=None)
+iData = pd.read_csv("DATA/translational_diff_coeff_molnum.txt", delim_whitespace=True, header=None)
 dTransCalc = float(iData.iloc[4,2])
 
 # Read in rotational values, calculate Tau, and average over x y z
-iDatax = pd.read_csv("../DATA/x_rotational_diff_coeff_moltyp.txt", delim_whitespace=True, header=None)
+iDatax = pd.read_csv("DATA/x_rotational_diff_coeff_moltyp.txt", delim_whitespace=True, header=None)
 val = iDatax.iloc[:7,2]
 mytime = iDatax.iloc[:7,1]
 out_tau_x = calc_tau(val, mytime)
 
-iDatay = pd.read_csv("../DATA/y_rotational_diff_coeff_moltyp.txt", delim_whitespace=True, header=None)
+iDatay = pd.read_csv("DATA/y_rotational_diff_coeff_moltyp.txt", delim_whitespace=True, header=None)
 val = iDatay.iloc[:7,2]
 mytime = iDatay.iloc[:7,1]
 out_tau_y = calc_tau(val, mytime)
 
-iDataz = pd.read_csv("../DATA/z_rotational_diff_coeff_moltyp.txt", delim_whitespace=True, header=None)
+iDataz = pd.read_csv("DATA/z_rotational_diff_coeff_moltyp.txt", delim_whitespace=True, header=None)
 val = iDataz.iloc[:7,2]
 mytime = iDataz.iloc[:7,1]
 out_tau_z = calc_tau(val, mytime)
